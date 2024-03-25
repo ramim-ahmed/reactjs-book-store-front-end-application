e61277f745560b8c22c0b560e2800d83369ae8c0
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-03-25 23:50:52 +0600

    [DONE] banner-section-ui

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import heroBook from '../assets/hero-img.png';
+export default function Hero() {
+  return (
+    <div className="bg-[#1313130D] lg:h-[500px] rounded-md flex lg:flex-row  justify-between flex-col items-center lg:p-20 p-10 my-8">
+      <div>
+        <h1 className="lg:text-5xl text-4xl font-playfair-display font-bold">Books to freshen up</h1>
+        <h1 className="lg:text-5xl text-4xl font-playfair-display font-bold">your bookshelf</h1>
+        <div className="mt-5 flex justify-center lg:justify-start">
+          <button className="bg-primary text-white px-9 py-2 text-lg font-medium">View The List</button>
+        </div>
+      </div>
+      <div className="mt-8 lg:mt-0">
+        <img src={heroBook} alt="" />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,11 @@
+import Hero from '@/components/Hero';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="m-3 lg:m-0">
+        <Hero />
+      </div>
+    </div>
+  );
 }
